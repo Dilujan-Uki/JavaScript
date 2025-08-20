ae2eb7dc5ce9ae2263a28950b6c752ab4ebed41f
Dilujan-Uki <dilujan.uki.stu@dreamspace.academy>
2025-08-20 15:20:42 +0530

    Today's lesson is function

---
+++ Name.js
@@ -0,0 +1,44 @@
+var name1 = "Jack";
+var name2 = "Sparrow";
+var name3 = "Jhon";
+var name4 = "Doe";
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
+console.log(name4);
+
+function printName(){
+    console.log("Hello"+name1);
+    console.log("Hello"+name2);
+    console.log("Hello"+name3);
+    console.log("Hello"+name4);
+}
+
+printName();
+
+var a = 10;
+var b = 30;
+
+function sum(){ 
+    console.log(a+b);
+}
+
+sum();
+
+function morningGreetings(){
+    console.log("Good Morning");
+}
+
+function eveningGreetings(){
+    console.log("Good Evening");
+}
+
+function greetings(){
+    morningGreetings();
+    eveningGreetings();
+}
+
+morningGreetings();
+eveningGreetings();
+greetings();
