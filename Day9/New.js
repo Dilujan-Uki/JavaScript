d4489af73a84073e95ff87a7c7631a5d25b8e2d0
Dilujan-Uki <dilujan.uki.stu@dreamspace.academy>
2025-09-03 17:09:42 +0530

    code for Palindrome

---
+++ Day9/New.js
@@ -0,0 +1,18 @@
+function sum(a,b){
+    return"Summation a+b is: "+(a+b);
+}
+console.log(sum(1,5));
+
+//Another one 
+let num1,num2,num3;
+num1=3;
+num2=3;
+num3=4;
+
+function summation(a,b){
+    let sum=(a+b);
+    return sum;
+}
+let output;
+output=summation(num1,num2);
+console.log("The total of num1+num2+num3 = "+(output+num3));
---
+++ Day9/palindrome.js
@@ -0,0 +1,17 @@
+//Palindrome 
+let str="amma"
+function isPalindrome(str){
+    let isPalin = true;
+    for(let i=0; i<(str.length/2); i++){
+        if(str.charAt(i)!=str.charAt(str.length-1-i))
+        isPalin=false;
+    }
+    return isPalin;
+}
+
+if(isPalindrome(str))
+    console.log("Yes, "+str+" is a Palindrome word");
+else
+    console.log("No, "+str+" isn't a Palindorme word");
+
+
