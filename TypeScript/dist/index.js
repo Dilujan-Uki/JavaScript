3385bd4c096ef86f3cde54075363df313bf50a57
Dilujan-Uki <dilujan.uki.stu@dreamspace.academy>
2025-10-13 16:49:15 +0530

    TypeScript First Day

--- TypeScript/dist/index.js
+++ TypeScript/dist/index.js
@@ -7,8 +7,8 @@ function main() {
     const student1 = new student_1.Student("Dilujan", 20, "Batticalo");
     console.log(student1.getStudentName());
     const student2 = new student_1.Student("Mukilan", 20, "Batticalo");
-    console.log("\n");
     console.log(student2.getStudentName());
+    console.log("\n");
     const radius1 = new circle_1.Circle(7);
     const radius2 = new circle_1.Circle(8);
     const radius3 = new circle_1.Circle(9);
@@ -16,9 +16,9 @@ function main() {
     console.log("The Area of Circle2 is " + Math.floor(radius2.getArea()));
     console.log("The Area of Circle3 is " + Math.floor(radius3.getArea()));
     console.log("\n");
-    console.log("The Perimiter of Circle1 is " + Math.floor(radius1.getArea()));
-    console.log("The Perimiter of Circle2 is " + Math.floor(radius2.getArea()));
-    console.log("The Perimiter of Circle3 is " + Math.floor(radius3.getArea()));
+    console.log("The Perimiter of Circle1 is " + Math.floor(radius1.getPerimiter()));
+    console.log("The Perimiter of Circle2 is " + Math.floor(radius2.getPerimiter()));
+    console.log("The Perimiter of Circle3 is " + Math.floor(radius3.getPerimiter()));
 }
 main();
 //# sourceMappingURL=index.js.map
--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -7,9 +7,8 @@ function main(){
     const student1 = new Student("Dilujan",20,"Batticalo");
      console.log(student1.getStudentName());
     const student2 = new Student("Mukilan",20,"Batticalo");
-
+     console.log(student2.getStudentName());
     console.log("\n");
-    console.log(student2.getStudentName());
     const radius1 = new Circle(7);
     const radius2 = new Circle(8);
     const radius3 = new Circle(9);
@@ -18,8 +17,8 @@ function main(){
     console.log("The Area of Circle3 is "+Math.floor(radius3.getArea()));
 
     console.log("\n")
-    console.log("The Perimiter of Circle1 is "+Math.floor(radius1.getArea()));
-    console.log("The Perimiter of Circle2 is "+Math.floor(radius2.getArea()));
-    console.log("The Perimiter of Circle3 is "+Math.floor(radius3.getArea()));
+    console.log("The Perimiter of Circle1 is "+Math.floor(radius1.getPerimiter()));
+    console.log("The Perimiter of Circle2 is "+Math.floor(radius2.getPerimiter()));
+    console.log("The Perimiter of Circle3 is "+Math.floor(radius3.getPerimiter()));
 }
 main();
